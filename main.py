cf52d359d0ac8bbb75c619e1db66aaf4d3cfd076
Dıjvar Ekinci <80560833+dijvar@users.noreply.github.com>
2023-05-17 17:36:04 +0300

    birden çok resim formatını aynı anda işleme. Hiç resim yoksa da hata vermesini engelleyip print ile bilgilendirme

--- main.py
+++ main.py
@@ -30,9 +30,15 @@ if __name__=="__main__":
 
     predictor = SamPredictor(sam)
     support_package = {"positive": {}, "negative": {}}
+
+    s_image_paths = []
     while True:
         # Support Images: Load, point, extract
-        s_image_paths = glob.glob(os.path.join(cfg.data.support_dir, f"*.{cfg.data.format}"))
+        data_format = cfg.data.format.split('|')
+
+        for i_data_dormat in data_format:
+            s_image_paths += glob.glob(os.path.join(cfg.data.support_dir, f"*.{i_data_dormat}"))
+        
         embeddings = []
         n_embeddings = []
         for sip in s_image_paths:
@@ -51,95 +57,102 @@ if __name__=="__main__":
                     n_embedding = utils.get_embedding(predictor, image, pt)
                     n_embeddings.append(n_embedding)
 
-        label, event = interface.relabel_or_continue()
-        support_package["positive"][label] = embeddings
-        support_package["negative"][label] = n_embeddings
-        if event == "END":
-            break
-        elif event == "CANCEL":
-            break
+        if len(s_image_paths) !=0:
+            label, event = interface.relabel_or_continue()
+            support_package["positive"][label] = embeddings
+            support_package["negative"][label] = n_embeddings
+            if event == "END":
+                break
+            elif event == "CANCEL":
+                break
+            else:
+                pass
         else:
-            pass
+            break
 
-    # Query Images: Load, extract, match
-    q_image_paths = glob.glob(os.path.join(cfg.data.query_dir, f"*.{cfg.data.format}"))
-    q_image_paths += glob.glob(os.path.join(cfg.data.query_dir, "**", f"*.{cfg.data.format}"))
-    q_image_paths += glob.glob(os.path.join(cfg.data.query_dir, "**", "**", f"*.{cfg.data.format}"))
-    q_image_paths += glob.glob(os.path.join(cfg.data.query_dir, "**", "**", "**", f"*.{cfg.data.format}"))
-    for cnt, qip in enumerate(q_image_paths):
-        masks = []
-        bboxes = []
-        labels_str = []
-        polygons = []
-        for label in support_package["positive"].keys():
-            print(f"[{cnt}/{len(q_image_paths)}] Loading {qip} >>>")
-            image = utils.import_image(qip)
-            q_image_shape = image.shape
-            image = cv2.resize(image, (cfg.window_size[0], cfg.window_size[1]))
-            predictor.set_image(image)
-            features = predictor.features
+    if len(s_image_paths) !=0:
 
-            similarity_maps = []
-            for i, embedding in enumerate(support_package["positive"][label]):
-                similarity_map = utils.get_similarity(embedding, features)
-                similarity_map = torch.where(similarity_map > cfg.threshold, 1., 0.)
-                similarity_maps.append(similarity_map)
-            for i, n_embedding in enumerate(support_package["negative"][label]):
-                similarity_map = utils.get_similarity(n_embedding, features)
-                similarity_map = torch.where(similarity_map > cfg.threshold, 1., 0.)
-                similarity_map = 1-similarity_map
-                similarity_maps.append(similarity_map)
+        # Query Images: Load, extract, match
+        q_image_paths = glob.glob(os.path.join(cfg.data.query_dir, f"*.{cfg.data.format}"))
+        q_image_paths += glob.glob(os.path.join(cfg.data.query_dir, "**", f"*.{cfg.data.format}"))
+        q_image_paths += glob.glob(os.path.join(cfg.data.query_dir, "**", "**", f"*.{cfg.data.format}"))
+        q_image_paths += glob.glob(os.path.join(cfg.data.query_dir, "**", "**", "**", f"*.{cfg.data.format}"))
+        for cnt, qip in enumerate(q_image_paths):
+            masks = []
+            bboxes = []
+            labels_str = []
+            polygons = []
+            for label in support_package["positive"].keys():
+                print(f"[{cnt}/{len(q_image_paths)}] Loading {qip} >>>")
+                image = utils.import_image(qip)
+                q_image_shape = image.shape
+                image = cv2.resize(image, (cfg.window_size[0], cfg.window_size[1]))
+                predictor.set_image(image)
+                features = predictor.features
 
-            similarity_maps = torch.stack(similarity_maps, dim=0)
-            print(f"HIGHEST SIMILARITY: {torch.max(similarity_maps).item()}")
-            similarity_maps = torch.einsum('bij->ij', similarity_maps)
-            print(f"HIGHEST SIMILARITY: {torch.max(similarity_maps).item()}")
+                similarity_maps = []
+                for i, embedding in enumerate(support_package["positive"][label]):
+                    similarity_map = utils.get_similarity(embedding, features)
+                    similarity_map = torch.where(similarity_map > cfg.threshold, 1., 0.)
+                    similarity_maps.append(similarity_map)
+                for i, n_embedding in enumerate(support_package["negative"][label]):
+                    similarity_map = utils.get_similarity(n_embedding, features)
+                    similarity_map = torch.where(similarity_map > cfg.threshold, 1., 0.)
+                    similarity_map = 1-similarity_map
+                    similarity_maps.append(similarity_map)
 
-            yx = (similarity_maps == torch.max(similarity_maps)).nonzero()
-            xy = utils.adapt_point(
-                {"x": yx[0, 1].item(), "y": yx[0, 0].item()},
-                initial_shape=features.shape[-2:],
-                final_shape=image.shape[0:2]
+                similarity_maps = torch.stack(similarity_maps, dim=0)
+                print(f"HIGHEST SIMILARITY: {torch.max(similarity_maps).item()}")
+                similarity_maps = torch.einsum('bij->ij', similarity_maps)
+                print(f"HIGHEST SIMILARITY: {torch.max(similarity_maps).item()}")
+
+                yx = (similarity_maps == torch.max(similarity_maps)).nonzero()
+                xy = utils.adapt_point(
+                    {"x": yx[0, 1].item(), "y": yx[0, 0].item()},
+                    initial_shape=features.shape[-2:],
+                    final_shape=image.shape[0:2]
+                )
+
+                l_ = np.ones((1,))
+                mask_, scores, logits = predictor.predict(
+                    point_coords=np.array([[xy["x"], xy["y"]]]).astype(int),
+                    point_labels=l_,
+                    multimask_output=True,
+                )
+                mask_ = mask_.astype(np.uint8)
+                mask_ = cv2.resize(mask_[0], (q_image_shape[1], q_image_shape[0]))
+
+                polygons = annotations.generate_polygons_from_mask(
+                    polygons=polygons,
+                    mask=mask_,
+                    label=label,
+                    polygon_resolution=cfg.labeling.polygon_resolution
+                )
+
+                masks.append(mask_)
+
+                # create xml file from the coordinates
+                coordinates = np.nonzero(mask_)
+                y0, y1, x0, x1 = coordinates[0].min(), coordinates[0].max(), coordinates[1].min(), coordinates[1].max()
+                bboxes.append([x0, y0, x1, y1])
+
+                labels_str.append(label)
+
+            masks_transformed = utils.merge_multilabel_masks(masks, COLORMAP=cfg.COLORMAP)
+
+            mask_path = qip.replace(
+                cfg.data.query_dir,
+                cfg.data.output_dir).replace(f".{cfg.data.format}", ".png")
+            mask_dir = "\\".join(mask_path.split("\\")[:-1])
+            if not os.path.isdir(mask_dir):
+                os.makedirs(mask_dir)
+
+            cv2.imwrite(
+                mask_path,
+                masks_transformed * 255
             )
 
-            l_ = np.ones((1,))
-            mask_, scores, logits = predictor.predict(
-                point_coords=np.array([[xy["x"], xy["y"]]]).astype(int),
-                point_labels=l_,
-                multimask_output=True,
-            )
-            mask_ = mask_.astype(np.uint8)
-            mask_ = cv2.resize(mask_[0], (q_image_shape[1], q_image_shape[0]))
-
-            polygons = annotations.generate_polygons_from_mask(
-                polygons=polygons,
-                mask=mask_,
-                label=label,
-                polygon_resolution=cfg.labeling.polygon_resolution
-            )
-
-            masks.append(mask_)
-
-            # create xml file from the coordinates
-            coordinates = np.nonzero(mask_)
-            y0, y1, x0, x1 = coordinates[0].min(), coordinates[0].max(), coordinates[1].min(), coordinates[1].max()
-            bboxes.append([x0, y0, x1, y1])
-
-            labels_str.append(label)
-
-        masks_transformed = utils.merge_multilabel_masks(masks, COLORMAP=cfg.COLORMAP)
-
-        mask_path = qip.replace(
-            cfg.data.query_dir,
-            cfg.data.output_dir).replace(f".{cfg.data.format}", ".png")
-        mask_dir = "\\".join(mask_path.split("\\")[:-1])
-        if not os.path.isdir(mask_dir):
-            os.makedirs(mask_dir)
-
-        cv2.imwrite(
-            mask_path,
-            masks_transformed * 255
-        )
-
-        annotations.create_xml_multilabel(image_name=qip, labels=labels_str, bboxes=bboxes)
-        annotations.create_polygon_json(image_path=qip, polygons=polygons, size=masks_transformed.shape)
+            annotations.create_xml_multilabel(image_name=qip, labels=labels_str, bboxes=bboxes)
+            annotations.create_polygon_json(image_path=qip, polygons=polygons, size=masks_transformed.shape)
+    else:
+        print("There is no image, please check your files")
