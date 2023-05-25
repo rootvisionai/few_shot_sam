50966d95c0bdba859f0ae94cc3188a79f57acd71
Tekin Evrim Ozmermer <evrimozmermer@hotmail.com>
2023-05-25 16:37:02 +0300

    deployed

--- frontend_python/make_request.py
+++ frontend_python/make_request.py
@@ -1,10 +1,10 @@
 import requests
 import json
 import interface_utils as utils
-
+import time
 
 # Define the URL of the endpoint: http://fewshotsam.rootvisionai.net
-url = "http://35.226.201.63:8080/extract_features"  # replace with your actual endpoint
+url = "http://fewshotsam.rootvisionai.net/extract_features"  # replace with your actual endpoint
 
 # Convert json to dict
 with open("request_content.json", "r") as fp:
@@ -16,9 +16,11 @@ headers = {
 }
 
 # Send the POST request
+t0 = time.time()
 response = requests.post(url, data=json.dumps(data_json), headers=headers)
+t1 = time.time()
 data_json = json.loads(response.text)
-image_path = "test.jpg"
+image_path = "../test.jpg"
 data_json["image_path"] = image_path
 init_image = utils.import_image(image_path)
 data_json["image"] = utils.numpy_to_base64(init_image)
@@ -27,12 +29,15 @@ data_json["image"] = utils.numpy_to_base64(init_image)
 print(data_json)
 
 # Define the URL of the endpoint
-url = "http://35.226.201.63:8080/generate/all"
+url = "http://fewshotsam.rootvisionai.net/generate/all"
 
 # Send the POST request
 response = requests.post(url, data=json.dumps(data_json), headers=headers)
+t2 = time.time()
 data_json = json.loads(response.text)
-print(data_json)
 with open("test.json", "w") as fp:
     json.dump(data_json["coco_json"], fp, indent=4)
+
+print(data_json)
+print(f"Request.1 Time: {t1-t0} | Request.2 Time: {t2-t1}")
 
